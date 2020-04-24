25194f7e15d4897c219db715531e5465714be7cf
outofobxthinking33 <outofobxthinking33@gmail.com>
2020-04-24 01:53:26 -0700

    single post page create

--- template-parts/content.php
+++ template-parts/content.php
@@ -4,6 +4,33 @@
 		
 		if ( is_single() ) :
 			the_title( '<h1 class="entry-title">', '</h1>' );
+
+			?>
+
+			<div class="single-post-header">
+				<div class="featured-image"><img src="<?php echo get_the_post_thumbnail_url( $post, 'full' ); ?>"></div>
+				<div class="post-info">
+					<?php 
+						global $post;
+
+						$authorID = get_post_field( 'post_author', $post->ID );
+
+						$authorName = get_the_author_meta( 'user_nicename', $authorID );
+
+						$authorAvatarUrl = get_avatar_url( $authorID, ['size' => 60] );
+
+						$category = get_the_category()[0]->name;
+					?>
+					<div class="authorAvatar"><img src="<?php echo $authorAvatarUrl; ?>"></div>
+					<div class="authorInfo">
+						<span class="authorName">by <?php echo $authorName; ?></span>
+						<span class="postDate"><?php echo get_the_date(); ?></span>
+					</div>
+				</div>
+			</div>
+
+			<?php
+
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
